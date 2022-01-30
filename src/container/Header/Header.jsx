0ffae0d648465a0d1ebcbefe0ac3a187c1ff16b7
sagar <chaudharysagar256@gmail.com>
2022-01-31 00:00:25 +0545

    Menuitems and special itemsadded

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-
+import { images } from '../../constants';
 import './Header.css';
+import { SubHeading } from '../../components';
 
-const Header = () => (
-  <div>
-    Header
-  </div>
-);
+const Header = () => {
+  return (
+    <div className='app__header app__wrapper section__padding' id='home'>
+      <div className="app__wrapper_info">
+        <SubHeading title="Chase the new flavour" />
+        <h1 className="app__header-h1">The Key to Fine Dining</h1>
+        <p className="p__opensans" style={{margin: '2rem 0'}}>stt tellus lobortis sed senectus vivarnus molestie, Condimentum volutpat morbi facilisis quam scelerisque sapien. Et penatibus amet tellus.</p>
+        <button type='button' className="custom__button">Explore More</button>
+      </div>
+
+      <div className="app__wrapper_img">
+        <img src={images.welcome} alt="welcome" />
+      </div>
+    </div>
+  )
+}
 
 export default Header;
